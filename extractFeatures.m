a53639ebc9258003b3e54dac81bbe3a7082d6da0
Morpheus3000 <Morpheus.morphia@gmail.com>
2016-03-24 12:48:45 +0100

    Feature Extractor
    Feature extractor with Class, Colour Spaces and Feature type

---
+++ extractFeatures.m
@@ -0,0 +1,14 @@
+function [Features, Descriptors] = extractFeatures(Class, featureType, colorSpace)
+    
+    
+    switch Class
+        case 'faces'
+
+        case 'airplanes'
+
+        case 'cars'
+
+        case 'motorbikes'
+    end
+
+end
