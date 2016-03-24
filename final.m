1da9290d4ccfdb1d3db6894fa9bf0f29b309f2e8
Ildefonso Pica <ildefonsofpica@gmail.com>
2016-03-24 11:28:21 +0100

    ""

--- final.m
+++ final.m
@@ -1,4 +1,5 @@
 %% Load images
+<<<<<<< Updated upstream
 planes = dir(fullfile('Caltech4','ImageData','airplanes_train','*.jpg'));
 planes = {planes.name}';
 %% Compute SIFT descriptors and perform k-means clustering
@@ -15,3 +16,41 @@ planesDarray = double(planesDarray);
 [planesC,planesA] = vl_kmeans(planesDarray,10);
 planesK = kmeans(planesDarray,10);
 %% Compute histograms
+=======
+% planes = dir(fullfile('Caltech4','ImageData','airplanes_train','*.jpg'));
+% planes = {planes.name}';
+faces = dir(fullfile('Caltech4','ImageData','faces_train','*.jpg'));
+faces = {faces.name}';
+%% Compute SIFT descriptors and perform k-means clustering
+n = 50;
+facesDlist = cell(n,1);
+for i = 1:n
+    face = single(rgb2gray(imread(faces{i})));
+    [~,Dface] = vl_sift(face);
+    facesDlist{i} = Dface;
+end
+facesDarray = [facesDlist{:}];
+facesDarray = double(facesDarray);
+% [minsize, minidx] = min(cellfun('size', facesDlist, 2));
+%% K-means clustering
+% We can compare the performance of the different k-means methods.
+[facesC, facesA] = vl_kmeans(facesDarray,400);
+% planesK = kmeans(planesDarray,100);
+Karray = facesC;
+%% Feature quantization
+test = single(rgb2gray(imread(faces{300}))); % Example histogram for one face image.
+[~,Dtest] = vl_sift(test); Dtest = double(Dtest);
+test_quant = zeros(size(Karray,2),1);
+dist = vl_alldist2(Dtest,Karray);
+for i = 1:size(Dtest,2)
+    [~,index] = min(dist(i,:));
+    test_quant(index) = test_quant(index)+1;
+end
+disp('Check!')
+figure; bar(test_quant./sum(test_quant))
+%% SVM for faces
+% Must use labeled data: -1 = negative example, +1 = positive example.
+for i = 1:n
+    test = single(rgb2gray(imread(faces{end-i})));
+end
+>>>>>>> Stashed changes
