90facb0fdd7a6c359b1e6101181a8a3aef248d50
caboj <jacob.verdegaal@student.uva.nl>
2016-03-28 20:40:21 +0200

    vocabsize results

--- experiment.m
+++ experiment.m
@@ -1,6 +1,6 @@
 load('shuffled.mat')
 voc_imgs=shuffled(1:200);
 train_imgs=shuffled(201:400);
-k=2000;
+k=200;
 colorSpace='gray';
 [faces,cars,motorbikes,airplanes,words]=bow(k,colorSpace,voc_imgs,train_imgs);
--- processResults.m
+++ processResults.m
@@ -1,9 +1,9 @@
-exps = { 'experiment1results.mat' 'experiment7results.mat' 'experiment8results.mat' 'experiment9results.mat' 'experiment10results.mat'};
+exps = { 'experiment11results.mat' 'experiment1results.mat' 'experiment7results.mat' 'experiment8results.mat' 'experiment9results.mat' 'experiment10results.mat'};
 
 map = zeros(4,5);
 ks = zeros(5,1);
 classes = {'faces' 'cars' 'motorbikes' 'airplanes'};
-for i = 1:5
+for i = 1:6
     load(char(exps(i)))
     ks(i) = k;
     for ci = 1:4
