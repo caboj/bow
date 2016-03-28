9ad60da4b5ce57b7e5824bcd29ba6b068c8d2f45
caboj <jacob.verdegaal@student.uva.nl>
2016-03-29 00:24:56 +0200

    final results 1

--- experiment.m
+++ experiment.m
@@ -2,7 +2,7 @@ load('shuffled.mat')
 voc_imgs=shuffled(1:200);
 train_imgs=shuffled(201:400);
 k=400;
-colorSpace='rgb';
+colorSpace='RGB';
 binsize = 0;
 kernel = 'polynomial 4';
 [faces,cars,motorbikes,airplanes,words]=bow(k,colorSpace,voc_imgs,train_imgs,binsize);
