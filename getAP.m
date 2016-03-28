7228895b947b8b7dd1e11030429a0cf7d839233b
caboj <jacob.verdegaal@student.uva.nl>
2016-03-28 14:20:03 +0200

    MAP ??

--- getAP.m
+++ getAP.m
@@ -1,15 +1,15 @@
-function [p,ap] = getAP(resStruct)
+function [p,map, ap] = getAP(resStruct)
     reals = resStruct.reals;
     predictions = resStruct.labels;
     
     n = size(predictions,1);
     ap = zeros(n,1);
-    ap(1) = .02*f(predictions(1),reals(1));
+    ap(1) = f(predictions(1),reals(1));
     
     for i = 2:n
-        ap(i) = .02* sum(ap(1:i-1))+f(predictions(i),reals(i))/i;
+        ap(i) = sum(ap(1:i-1))+f(predictions(i),reals(i))/i;
     end
-    
+    map = (1/50)*ap(n);
     p = sum(predictions == reals)/n;
 end
 
