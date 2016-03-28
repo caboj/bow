4f0d73bd1401839f0aa0dfc0bc85e38f2142369d
caboj <jacob.verdegaal@student.uva.nl>
2016-03-28 16:53:41 +0200

    more results

--- experiment.m
+++ experiment.m
@@ -2,5 +2,5 @@ load('shuffled.mat')
 voc_imgs=shuffled(1:200);
 train_imgs=shuffled(201:400);
 k=1000;
-colorSpace='gray';
+colorSpace='rgb';
 [faces,cars,motorbikes,airplanes,words]=bow(k,colorSpace,voc_imgs,train_imgs);
